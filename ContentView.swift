0137b099cfabd5c634b7a00d3869532179030125
Oleksandr Andrusyshyn <oandrusyshyn7497@stu.d214.org>
2026-09-14 11:17:34 -0500

    Made list diplaying an array

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tasks = ["Hello","It's me","It's Verity"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List{
+            ForEach(tasks,id: \.self){ taskIs in
+                Text(taskIs)
+            }
         }
     }
 }
