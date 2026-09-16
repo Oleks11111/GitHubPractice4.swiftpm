1c83b240b3fb7060580ccd4f8534f20817dc5132
Akram Aloviddinov <akalov09@gmail.com>
2026-09-16 11:07:52 -0500

    cleared and connected tasks and added HeaderView

--- ContentView.swift
+++ ContentView.swift
@@ -1,15 +1,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks: [Task] = [
-        Task(nameOfTask:"Buy Milk"),
-        Task(nameOfTask:"Buy Eggs"),
-        Task(nameOfTask:"Buy Bread"),
-    ]
+    @State var tasks: [Task] = []
     var body: some View {
-        ForEach(tasks, id: \.self){ currentTask in Text(currentTask.nameOfTask)}
-    }
+           
+        HeaderView(tasks: $tasks)
+            
+            
+            
+            List(tasks, id: \.self){ currentTask in Text(currentTask.nameOfTask)}
+        }
+        
     
 }
-
 
