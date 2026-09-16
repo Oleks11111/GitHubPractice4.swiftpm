7a8a89b535a487fe62f410befe48ce3240ca8022
Oleksandr Andrusyshyn <oandrusyshyn7497@stu.d214.org>
2026-09-16 10:17:46 -0500

    Created a HeaderView with binding of task array and a textfield to enter a task that will be added to a list by a button.

---
+++ HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  GitHubPractice4
+//
+//  Created by Oleksandr Andrusyshyn on 9/16/26.
+//
+import SwiftUI
+struct HeaderView: View {
+    @Binding var tasks: [Task]
+    @State var typeIn = ""
+    var body: some View{
+        TextField("Enter a task", text: $typeIn)
+        Button("Add"){
+            tasks.append(typeIn)
+        }
+    }
+    
+}
